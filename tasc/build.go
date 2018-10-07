b445546263646d3f28c579e59cd2193a8b33711e
Drew Ripberger <drew.ripberger@gmail.com>
2018-10-07 00:40:17 -0400

    Added manif gen

--- tasc/build.go
+++ tasc/build.go
@@ -5,6 +5,8 @@ import (
     "io/ioutil"
     "crypto/sha256"
     "encoding/hex"
+    "encoding/json"
+    "fmt"
 )
 
 func CreateDir(){
@@ -30,6 +32,31 @@ func BuildStories(){
     }
 }
 
-/*func (b *Branch) GenManifest(){
-
-}*/
+func (b *Branch) ReplaceWithHashed(){
+    // Recursive base case
+    if b.TextPath == "END" && b.Choice == "END" {
+        return
+    }
+    content, _ := ParseFile(b.TextPath)
+    hash := sha256.Sum256([]byte(content))
+    b.TextPath = hex.EncodeToString(hash[:])
+    // For each option listed in a tam file, parse into a Branch struct
+    for i := 0; i < len(b.Branches); i++ {
+        b.Branches[i].ReplaceWithHashed()
+    }
+}
+func (b *Branch) GenManifest(){
+    b.ReplaceWithHashed()
+    fmt.Println(b)
+    j, err := json.Marshal(b)
+    if err != nil {
+        panic(err)
+    }
+    f, err := os.Create(BuildPath + string(os.PathSeparator) + "map.json")
+    if err != nil {
+        panic(err)
+    }
+    f.WriteString(string(j))
+    f.Sync()
+    f.Close()
+}
